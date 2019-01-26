408b6cb31ea77a1865d20f736ecab5ec1006793d
Jean Pignouf <maxime.de-la-fouchardiere@epitech.eu>
2019-01-26 20:37:26 +0200

    Gave up on importing scripts, building the Form object with it's error handling

---
+++ html/Form.js
@@ -0,0 +1,5 @@
+const FormTypes = ["Circle", "Line", "Rectangle", "Text"];
+
+export {
+    FormTypes
+};
--- html/main.js
+++ html/main.js
@@ -1,13 +1,73 @@
+function getCanvasName() {
+    // Polyfill degeulasse pour que ca marche sur IE...
+    document.currentScript = document.currentScript || (function() {
+        var scripts = document.getElementsByTagName('script');
+        return scripts[scripts.length - 1];
+    })();
+    
+    // Getting the canvas name from the argument given to the script tag
+    return document.currentScript.getAttribute('canvasName');
+}
 
-// Polyfill degeulasse pour que ca marche sur IE...
-document.currentScript = document.currentScript || (function() {
-    var scripts = document.getElementsByTagName('script');
-    return scripts[scripts.length - 1];
-})();
+const FormTypes = ["Dot", "Line"]
 
-// Getting the canvas name from the argument given to the script tag
-const canvasName = document.currentScript.getAttribute('canvasName');
+function FormCheckParam(tag, params, keyname, proof)
+{
+    var value;
 
+    if (params == undefined) {
+        console.error(`[${tag}] - Empty params`);
+        return false;
+    }
+
+    if (params.hasOwnProperty(keyname))
+        value = params[keyname];
+    else {
+        console.error(`[${tag}] - Missing ${keyname}`);
+        return false;
+    }
+
+    // No validation function
+    if (proof === undefined) {
+        return true;
+    }
+
+    if (proof(value) == true)
+        return true;
+    else {
+        console.error(`[${tag}] - Invalid key ${keyname} : ${value}`)
+    }
+}
+
+function FormValidDotParams(params) {
+    if (!FormCheckParam("Form.Dot.Valid", params, "x", undefined))
+        return false;
+    if (!FormCheckParam("Form.Dot.Valid", params, "y", undefined))
+        return false;
+    return true;
+}
+
+function Form(type, params) {
+
+    // Checking if given type is valid
+    if (typeof type != 'string') {
+        console.error(`[Form] - Expected a string as first argument but got : [${type}]`);
+        return;
+    }
+    if (!FormTypes.includes(type)) {
+        console.error(`[Form] - Expected FormTypes:[${FormTypes}] but got : [${type}]`);
+        return;
+    }
+    this.type = type;
+
+    // Same for parameters
+    if (type == "Dot" && !FormValidDotParams(params)) {
+        console.log("Invalid Params : ", params);
+        return;
+    }
+}
+
+// Our Logmond declaration
 function Logmond(canvasRef) {
     this.canvas = canvasRef;
     if (!this.canvas) {
@@ -20,7 +80,6 @@ function Logmond(canvasRef) {
     this.dragging = false;
     
     this.canvas.addEventListener('mousedown', function(e) {
-        console.log("Press !");
         this.dragging = true;
         this.ctx = canvasRef.getContext("2d");
         this.ctx.beginPath();
@@ -29,17 +88,17 @@ function Logmond(canvasRef) {
     }, false)
     
     this.canvas.addEventListener('mouseup', function(e) {
-        console.log("Release !");
         this.dragging = false;
     })
     
     this.canvas.addEventListener('mousemove', function(e) {
         if (this.dragging) {
-            console.log("YOOO");
             this.ctx.lineTo(e.offsetX, e.offsetY);
             this.ctx.stroke();
         }
     })
 }
 
-const obj = new Logmond(document.getElementById(canvasName))
+const obj = new Logmond(document.getElementById(getCanvasName()))
+const test = new Form("Dot", {time: "31h2", x: 45, y: 67});
+console.log("Types : ", FormTypes);

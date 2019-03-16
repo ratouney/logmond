b2a699243e6add5113b60c0e7b80c05edadea8a4
Jean Pignouf <ratouney1998@gmail.com>
2019-03-16 18:17:34 +0100

    Added first constructions for linking

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var shapes = [];
 var selected = null;
 var dragging = false;
 var resizing = false;
-var needUpdate = false;
+var needUpdate = true;
 var inCanvas = true;
 
 var mouseDownOffset = {
@@ -25,6 +25,9 @@ var mouseDownOffset = {
 }
 
 function refreshCanvas() {
+    if (!needUpdate)
+        return;
+
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = "rgb(255, 255, 255)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -32,30 +35,199 @@ function refreshCanvas() {
     shapes.forEach((elem) => {
         drawCircle(ctx, elem);
     })
+
+    if (shapes.length > 1) {
+        var last = null;
+        shapes.forEach((elem) => {
+            if (last == null)
+                last = elem.id;
+            else {
+                drawLink(ctx, last, elem.id);
+                last = elem.id;
+            }
+        })
+    }
+
+    needUpdate = false;
+}
+
+function drawLine(dot, equ) {
+    const ctx = canvas.getContext('2d')
+
+    var leftSide = getYFromEquation(0, equ);
+    var rightSide = getYFromEquation(canvas.width, equ);
+
+    ctx.moveTo(0, leftSide);
+    ctx.lineTo(dot.x, dot.y);
+    ctx.lineTo(canvas.width, rightSide);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawLineBetweenDots(dot, blep) {
+    const equ = getEquation(dot, blep);
+    drawLine(dot, equ);
+}
+
+function getEquation(dot, blep) {
+    const a = (dot.y - blep.y) / (dot.x - blep.x)
+    const b = (a * (dot.x) - dot.y) * -1
+
+    return {
+        a: a,
+        b: b
+    };
+}
+
+function getDistance(dot, blep) {
+    return Math.sqrt(Math.pow(dot.x - blep.x, 2) + Math.pow(dot.y - blep.y, 2))
+}
+
+function getPerpendicularEquation(dot, equ)
+{
+    const a = -1 / equ.a;
+    const b = (a * dot.x - dot.y) * -1;
+
+    return {
+        a: a,
+        b: b
+    }
+}
+
+function getYFromEquation(xcord, equ) {
+    return (equ.a * xcord) + equ.b
+}
+
+function getIntersection(equ, line) {
+    if (equ.a == line.a && equ.b != line.b) {
+        // Les coefs de direction sont les mêmes donc les droites sont parallèles
+        // Si la hauteur est pareil, les droites sont superposées
+        console.error("IDENTICAL COEFS")
+        return null;
+    }
+
+    const x = (line.b - equ.b) / (equ.a - line.a)
+    const y = equ.a * x + equ.b 
+
+    return {
+        x: x,
+        y: y,
+    }
+}
+
+function drawLink(ctx, firstId, secondId) {
+    console.log(`Draw links between ${firstId} and ${secondId}`)
+
+    const idx1 = shapes.findIndex(elem => elem.id == firstId);
+    const elem1 = shapes[idx1]
+
+    const idx2 = shapes.findIndex(elem => elem.id == secondId);
+    const elem2 = shapes[idx2]
+
+    const a = {
+        x: elem1.config.center.x,
+        y: elem1.config.center.y
+    }
+
+    const b = {
+        x: elem2.config.center.x,
+        y: elem2.config.center.y
+    }
+
+    console.log("A : ", a)
+    console.log("B : ", b)
+
+    const ab = getEquation(a, b)
+    
+    console.log("Equation AB : ", ab)
+
+    drawLine(a, ab);
+
+    // https://forums.futura-sciences.com/mathematiques-college-lycee/376246-pente-dune-fonction-de-langle-abscices.html
+
+    const angle = (Math.PI * -0.5) + Math.atan(ab.a);
+    /*
+    console.log("Angle : ", angle)
+    console.log("AngleDeg: ", angle * 180 / Math.PI)
+    console.log("EquAngle : ", Math.atan(ab.a))
+    console.log("EquAngleDeg : ", Math.atan(ab.a) * 180 / Math.PI)
+    */
+
+    const m = {
+        x: a.x + (elem1.config.radius * Math.cos(angle)),
+        y: a.y + (elem1.config.radius * Math.sin(angle))
+    }
+
+    console.log("M : ", m)
+
+    drawDot(m, 3);
+    drawLineBetweenDots(a, m);
+
+    const n = {
+        x: b.x + (elem2.config.radius * Math.cos(angle)),
+        y: b.y + (elem2.config.radius * Math.sin(angle))  
+    }
+
+    drawDot(n, 3);
+    drawLineBetweenDots(b, n);
+
+    const topLine = getEquation(m, n);
+
+    drawLine(m, topLine);
+
+    const o = getIntersection(ab, topLine)
+
+    console.log("O : ", o)
+
+    drawDot(o, 5);
+
+    const circle = canvas.getContext('2d')
+
+    const oamiddle = {
+        x: (o.x + a.x) / 2,
+        y: (o.y + a.y) / 2,
+    }
+
+    circle.beginPath();
+    circle.arc(oamiddle.x, oamiddle.y, getDistance(o, a) / 2, 0, 2 * Math.PI);
+    circle.stroke();
+    circle.closePath();
+}
+
+function drawDot(dot, rad) {
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = "#000000"
+    ctx.beginPath();
+    ctx.arc(dot.x, dot.y, rad, 0, 2 * Math.PI)
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 }
 
 function drawCircle(context, circle) {
     context.fillStyle = circle.config.color || "#FFFFFF";
     context.strokeStyle = circle.config.borderColor || "#FFFFFF";
-    context.lineWidth = 7;
+    //context.lineWidth = 7;
     context.beginPath();
     context.arc(circle.config.center.x, circle.config.center.y, circle.config.radius, 0, 2 * Math.PI);
     context.stroke();
     context.fill();
+    context.closePath()
 }
 
 canvas.addEventListener('mousedown', function(e) {
-    console.log("Click !");
+    //console.log("Click !");
     const drawOrder = shapes.map(x => x).reverse()
     const touched = drawOrder.findIndex((elem) => {
-        console.log("Checking circle : ", elem.id)
+        //console.log("Checking circle : ", elem.id)
 
         const { config: { center: {x, y} , radius } } = elem;
 
         const distance = Math.sqrt(Math.pow(e.offsetX - x, 2) + Math.pow(e.offsetY - y, 2))
 
         if (Math.abs(distance - radius) < 3) {
-            console.log("You are on the edge ! Modify size !")
+            //console.log("You are on the edge ! Modify size !")
             resizing = true;
             return true;
         }
@@ -83,7 +255,6 @@ canvas.addEventListener('mousedown', function(e) {
 })
 
 canvas.addEventListener('mouseup', function(e) {
-    console.log("Nuh !");
     if (dragging) {
         dragging = false;
     }
@@ -92,17 +263,18 @@ canvas.addEventListener('mouseup', function(e) {
 
         shapes[idx].config.borderColor = shapes[idx].config.originalBorderColor
         resizing = false;
+        needUpdate = true;
     }
     selected = null;
 })
 
 canvas.addEventListener('mouseout', function(e) {
+    //console.log("I'm out !");
     inCanvas = false;
-    console.log("I'm out !");
 })
 
 canvas.addEventListener('mouseenter', function(e) {
-    console.log("I'm back !");
+    //console.log("I'm back !");
     inCanvas = true;
 })
 
@@ -111,7 +283,7 @@ document.addEventListener('mousemove', function(e) {
     const mouseY = e.offsetY
 
     if (selected != null && dragging && inCanvas) {
-        console.log("Move circle : ", selected)
+        //console.log("Move circle : ", selected)
 
         const idx = shapes.findIndex(elem => elem.id == selected);
 
@@ -158,6 +330,7 @@ shapes.push({
         radius: 150
     }
 })
+/*
 shapes.push({
     id: "thirdCircle",
     config: {
@@ -171,5 +344,6 @@ shapes.push({
         radius: 130
     }
 })
+*/
 
 setInterval(refreshCanvas, 1000 / refreshRate);
